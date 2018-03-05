3adc389391c9be3ff3ab39f774c69fb9144a1fa1
Richard Coleman <richard@microbug.uk>
2018-03-05 18:40:10 +0000

    Fix download, improve messages

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ else
     write_red_bold "Accept all prompts and provide your password when it's asked for"
     HOMEBREW_INSTALL_SCRIPT=$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install) || exit 1
     /usr/bin/ruby -e "$HOMEBREW_INSTALL_SCRIPT"
+    brew analytics off
 fi
 
 print_step 2
@@ -56,7 +57,7 @@ brew install winetricks
 
 print_step 8
 write_bold "Creating virtual Windows installation at ~/pokemon_uranium"
-write_red_bold "Remember to accept all prompts to install Mono and/or Gecko"
+write_red_bold "Remember to accept all prompts to install Mono and/or Gecko, you may be asked several times"
 write_bold "Lots of Wine logs (may look like nonsense) coming up..."
 export PATH=$PATH:"/Applications/Wine Staging.app/Contents/Resources/wine/bin/"
 mkdir ~/pokemon_uranium
@@ -64,13 +65,17 @@ export WINEPREFIX=~/pokemon_uranium
 cd $WINEPREFIX || exit
 wineboot
 winetricks directplay directmusic dsound d3dx9_43 ddr=opengl macdriver=x11 win10 devenum dmsynth quartz
-# Remove cache files
-rm -rf ~/.cache/wine ~/.cache/winetricks
 sleep 5  # Let Wine finish spewing logs
 
 print_step 9
 write_bold "Adding game start script"
-curl -s -O "$HOME/pokemon_uranium/Run Pokémon Uranium.command" "https://raw.githubusercontent.com/microbug/pokemon-uranium-on-macos/master/run.sh"
+curl -s -o "$HOME/pokemon_uranium/Run Pokémon Uranium.command" "https://raw.githubusercontent.com/microbug/pokemon-uranium-on-macos/master/run.sh"
 
-write_bold "Done, now REBOOT and check the guide on GitHub for next steps"
+print_step 10
+write_bold "Clearing caches"
+rm -rf ~/.cache/wine ~/.cache/winetricks
+rm -rf $(brew --cache)
+
+write_red_bold "Done!"
+write_red_bold "Wait for all Wine configuration to finish (wait for any remaining windows to close), then REBOOT and check the guide on GitHub for next steps"
 
